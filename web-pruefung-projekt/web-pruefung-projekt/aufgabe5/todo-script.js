8993fab8e2b09c1fecdd1e5ed6ac5f6f340041fb
Manuel <manuel.treder@live.com>
2025-02-08 20:51:31 +0100

    Klausur und Aufgabe 3

---
+++ web-pruefung-projekt/web-pruefung-projekt/aufgabe5/todo-script.js
@@ -0,0 +1,11 @@
+// Hier den Code ergaenzen 
+// (Event-Handling fuer Hinzufuegen-Button, DOM-Manipulation fuer Tabelle)
+
+function createCheckbox(tr) {
+  // Hier den Code fuer die Erzeugung der Checkbox ergaenzen
+  // Empfohlene Strategie:
+  // 1. Erstelle eine Checkbox per DOM-API
+  // 2. Fuege einen Event-Handler fuer Klick auf die Checkbox hinzu
+  // 3. Falls Checkbox angehakt (checked): Text in Zeile (tr) durchstreichen
+  // 4. Sonst: Text in Zeile (tr) normal darstellen
+}
---
+++ web-pruefung-projekt/web-pruefung-projekt/aufgabe6/models/bilder.js
@@ -0,0 +1,21 @@
+const bilder = [
+  {
+    dateiname: "robot.png",
+    beschreibung: "Frida Kahlo painting of a robot waving goodbye",
+  },
+  {
+    dateiname: "meadow.png",
+    beschreibung:
+      "A photo of a wide green meadow and a clear blue sky",
+  },
+  {
+    dateiname: "elephant.png",
+    beschreibung: "A pink elephant in a small room",
+  },
+  {
+    dateiname: "fhdortmund.png",
+    beschreibung: "An expressive oil painting of FH Dortmund in summer",
+  },
+];
+
+// Hier den Code ergaenzen!
