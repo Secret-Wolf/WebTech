2797f817158d8a323a5f5d719dce98f2e8f77edc
Manuel <manuel.treder@live.com>
2025-02-08 22:08:55 +0100

    Aufgabe4

--- Klausur/aufgabe4/js-objects.js
+++ Klausur/aufgabe4/js-objects.js
@@ -1,1 +1,14 @@
 // Hier den Code ergaenzen!
+let webtech = {
+    credits: 5,
+    title: "Web-Technologien",
+    pruefung: { hilfsmittelErlaubt: true,
+        maxPunkte: 100
+    }
+}
+
+let klausur = {dauer: 120, hilfsmittelErlaubt: false}
+
+Object.setPrototypeOf(webtech, klausur);
+
+console.log(webtech.dauer);
